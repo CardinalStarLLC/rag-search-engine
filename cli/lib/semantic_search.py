77c3fe2f6304c0790d884e2a87438bc4bb6c86ca
CardinalStarLLC <60763991+CardinalStarLLC@users.noreply.github.com>
2026-01-14 23:04:51 -0500

    CH4-Lesson10

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,12 +1,27 @@
+import numpy as np
 from sentence_transformers import SentenceTransformer
+
+def embed_text(text):
+    sm = SemanticSearch()
+    embedding = sm.generate_embedding(text)
+
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
 
 class SemanticSearch:
     def __init__(self) -> None:
         # Load the model (downloads automatically the first time)
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
 
+    def generate_embedding(self, text):
+        if text is None or text.strip() == "":
+            raise ValueError("Input text must be a non-empty string.")
+        
+        sentences = [text]
+        embedding = self.model.encode(sentences)
+        return embedding[0]
+
     def verify_model(self) -> None:
         print(f"Model loaded: {self.model}")
         print(f"Max sequence length: {self.model.max_seq_length}")
-
-    #sm.model.encode(text)
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import SemanticSearch
+from lib.semantic_search import *
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     subparsers.add_parser("verify", help="Verify the semantic search model")
+    
+    embed = subparsers.add_parser("embed_text", help="Verify the semantic search model")
+    embed.add_argument("text", type=str, help="Text to embed")
 
     args = parser.parse_args()
 
     match args.command:
+        case "embed_text":
+            embed_text(args.text)
         case "verify":
             sm = SemanticSearch()
             sm.verify_model()
